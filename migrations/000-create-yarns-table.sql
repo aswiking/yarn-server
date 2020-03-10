e815e538006cba44214676dc6a4f7cc4ef8d45a0
Sofia Wiking <aswiking@gmail.com>
2020-03-10 08:47:36 +0000

    create migrations and seeds

---
+++ migrations/000-create-yarns-table.sql
@@ -0,0 +1,7 @@
+CREATE TABLE yarns (
+    id SERIAL PRIMARY KEY,
+    name TEXT NOT NULL,
+    brand TEXT NOT NULL,
+    weight TEXT,
+    fibers TEXT
+);
---
+++ seeds/seed-yarns.sql
@@ -0,0 +1,8 @@
+INSERT INTO yarns (name, brand, weight, fibers)
+VALUES
+
+
+('Drops Alpaca','Drops', 'Sport', '100% alpaca'),
+('Felted Tweed','Rowan','DK','50% wool, 25% alpaca, 25% rayon'),
+('Sparrow', 'Quince and Co', 'Fingering', '100% linen'),
+('Gloss Fingering', 'Knit Picks', 'Fingering', '70% wool, 30% silk')
